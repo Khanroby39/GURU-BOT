6fbbb918da1b2b677b377dd28de263537d0051b7
Khanroby39 <159727656+Khanroby39@users.noreply.github.com>
2024-02-12 02:57:10 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
+  ['923264791203', 'Khan_Roby_39', true],
   ['6281255369012', 'Ash', true], 
   [''] 
 ] //Number of owners
